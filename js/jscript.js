ef5c5c591da2453f68cf185ee6739fe5083258f8
Thanatus <s.sidoryaka@gmail.com>
2020-12-08 17:50:22 +0200

    fix viber

--- js/jscript.js
+++ js/jscript.js
@@ -1,1 +1,15 @@
 console.log('Work');
+
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  // код для мобильных устройств
+  const viber = document.querySelector('#viber');
+  console.log('Mobila');
+  viber.href = 'viber://add?number=380503283636';
+} else {
+  // код для обычных устройств
+  console.log('PC');
+}
